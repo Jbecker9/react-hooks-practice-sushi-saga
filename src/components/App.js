0cc82ac521e2fb2fee1cf77e59771478150867e3
Jared <jbeckersalax9@aol.com>
2022-02-18 19:50:25 -0500

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
-const API = "http://localhost:3001/sushis";
 
 function App() {
+  const [sushis, setSushis] = useState([])
+
+  function renderSushi(prop){
+    setSushis(prop)
+  }
+
   return (
     <div className="app">
-      <SushiContainer />
+      <SushiContainer sushis={sushis} renderSushi={(prop)=>renderSushi(prop)}/>
       <Table />
     </div>
   );
--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import MoreButton from "./MoreButton";
+import Sushi from "./Sushi"
 
-function SushiContainer(props) {
+function SushiContainer({ sushis, renderSushi }) {
+  const [count, setCount] = useState(0)
+
+  useEffect(()=>{
+    fetch("http://localhost:3001/sushis")
+      .then((r)=>r.json())
+      .then((sushiData)=>renderSushi(sushiData.slice(count,4)))
+  }, [])
+
+  console.log(count)
+
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
-      <MoreButton />
+      {sushis.map((sushiMap)=><Sushi key={sushiMap.id} sushiMap={sushiMap}/>)}
+      <MoreButton count={count} />
     </div>
   );
 }
